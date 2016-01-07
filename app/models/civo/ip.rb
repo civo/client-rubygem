96492113bbaec6228f7a69724d29d65db0acd342
Andy Jeffries <andy@andyjeffries.co.uk>
2016-01-07 09:09:40 +0000

    Commenting out IP methods as the IP API isn't finished yet and the the interface may change

--- app/models/civo/ip.rb
+++ app/models/civo/ip.rb
@@ -1,12 +1,12 @@
 module Civo
   class Ip < Base
-    get :for_instance, "/v1/instances/:id/ip", requires: [:id],
-      fake: [ { "private_ip": "10.0.0.1" }, { "private_ip": "10.0.0.1", "public_ip": "31.28.96.101" }]
-    post :create, "/v1/instances/:id/ip", requires: [:id],
-      fake: { "result": "ok" }
-    delete :remove, "/v1/instances/:id/ip/:ip_address", requires: [:id, :ip_address],
-      fake: { "result": "ok" }
-    put :connect, "/v1/instances/:id/ip/:ip_address", requires: [:id, :ip_address],
-      fake: { "result": "ok" }
+    # get :for_instance, "/v1/instances/:id/ip", requires: [:id],
+    #   fake: [ { "private_ip": "10.0.0.1" }, { "private_ip": "10.0.0.1", "public_ip": "31.28.96.101" }]
+    # post :create, "/v1/instances/:id/ip", requires: [:id],
+    #   fake: { "result": "ok" }
+    # delete :remove, "/v1/instances/:id/ip/:ip_address", requires: [:id, :ip_address],
+    #   fake: { "result": "ok" }
+    # put :connect, "/v1/instances/:id/ip/:ip_address", requires: [:id, :ip_address],
+    #   fake: { "result": "ok" }
   end
 end
