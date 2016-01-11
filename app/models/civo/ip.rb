628b8d2509941fc3f2e0565dcd720d5dca0c0c03
Andy Jeffries <andy@andyjeffries.co.uk>
2016-01-11 11:15:04 +0000

    Added IP calls in to Civo

--- app/models/civo/ip.rb
+++ app/models/civo/ip.rb
@@ -1,12 +1,7 @@
 module Civo
   class Ip < Base
-    # get :for_instance, "/v1/instances/:id/ip", requires: [:id],
-    #   fake: [ { "private_ip": "10.0.0.1" }, { "private_ip": "10.0.0.1", "public_ip": "31.28.96.101" }]
-    # post :create, "/v1/instances/:id/ip", requires: [:id],
-    #   fake: { "result": "ok" }
-    # delete :remove, "/v1/instances/:id/ip/:ip_address", requires: [:id, :ip_address],
-    #   fake: { "result": "ok" }
-    # put :connect, "/v1/instances/:id/ip/:ip_address", requires: [:id, :ip_address],
-    #   fake: { "result": "ok" }
+    post :create, "/v1/instances/:id/ip", requires: [:id], defaults: {public: true}
+    delete :remove, "/v1/instances/:id/ip/:ip_address", requires: [:id, :ip_address]
+    put :connect, "/v1/instances/:id/ip/:ip_address", requires: [:id, :ip_address, :private_ip]
   end
 end
