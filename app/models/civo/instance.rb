2dea8efd8ba2a9c720abcca7b26eb4998c5032e5
Andy Jeffries <andy@andyjeffries.co.uk>
2016-01-13 12:11:36 +0000

    Added stop and start commands for instances

--- app/models/civo/instance.rb
+++ app/models/civo/instance.rb
@@ -5,6 +5,9 @@ module Civo
       defaults: {public_ip: true, template: "ubuntu-14.04", initial_user: "civo"}
     delete :remove, "/v1/instances/:id", requires: [:id]
     post :reboot, "/v1/instances/:id/reboots", requires: [:id]
+    put :stop, "/v1/instances/:id/stop", requires: [:id]
+    put :start, "/v1/instances/:id/start", requires: [:id]
+    post :reboot, "/v1/instances/:id/reboots", requires: [:id]
     put :upgrade, "/v1/instances/:id", requires: [:size, :id]
 
     def ip_addresses
