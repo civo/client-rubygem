a6065d6fcc72540e3fcba1a6469b3250b09a62e3
Andy Jeffries <andy@andyjeffries.co.uk>
2016-01-15 16:42:30 +0000

    Added hard/soft reboot methods rather just a reboot

--- app/models/civo/instance.rb
+++ app/models/civo/instance.rb
@@ -6,6 +6,8 @@ module Civo
       defaults: {public_ip: true, template: "ubuntu-14.04", initial_user: "civo"}
     delete :remove, "/v1/instances/:id", requires: [:id]
     post :reboot, "/v1/instances/:id/reboots", requires: [:id]
+    post :hard_reboot, "/v1/instances/:id/hard_reboots", requires: [:id]
+    post :soft_reboot, "/v1/instances/:id/soft_reboots", requires: [:id]
     put :stop, "/v1/instances/:id/stop", requires: [:id]
     put :start, "/v1/instances/:id/start", requires: [:id]
     post :reboot, "/v1/instances/:id/reboots", requires: [:id]
--- lib/civo/version.rb
+++ lib/civo/version.rb
@@ -1,3 +1,3 @@
 module Civo
-  VERSION = "0.3.0"
+  VERSION = "0.3.1"
 end
