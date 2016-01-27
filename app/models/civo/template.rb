91513ac9812cdd01daa2008e23a7e3462f2f4c60
Andy Jeffries <andy@andyjeffries.co.uk>
2016-01-27 13:12:00 +0000

    Added required fields on template creation and template details call

--- app/models/civo/template.rb
+++ app/models/civo/template.rb
@@ -1,8 +1,9 @@
 module Civo
   class Template < Base
     get :all, "/v1/templates"
+    get :details, "/v1/templates/:id"
     put :save, "/v1/templates/:id"
-    post :create, "/v1/templates"
+    post :create, "/v1/templates", required: [:name, :image_id]
     delete :remove, "/v1/templates/:id"
   end
 end
--- lib/civo/version.rb
+++ lib/civo/version.rb
@@ -1,3 +1,3 @@
 module Civo
-  VERSION = "0.3.7"
+  VERSION = "0.3.8"
 end
