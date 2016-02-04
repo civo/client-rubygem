89ec5f16bd70a156996d7c1a3b49c809786bd2a7
Andy Jeffries <andy@andyjeffries.co.uk>
2016-02-04 11:21:47 +0000

    Fixed Firewall create API

--- app/models/civo/firewall.rb
+++ app/models/civo/firewall.rb
@@ -1,7 +1,7 @@
 module Civo
   class Firewall < Base
     get :all, "/v1/firewalls"
-    put :create, "/v1/firewalls/:name", required: [:name]
+    post :create, "/v1/firewalls", required: [:name]
     delete :remove, "/v1/firewalls/:name", required: [:name]
   end
 end
--- lib/civo/version.rb
+++ lib/civo/version.rb
@@ -1,3 +1,3 @@
 module Civo
-  VERSION = "0.3.14"
+  VERSION = "0.3.15"
 end
