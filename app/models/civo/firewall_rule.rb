240fe28cf6d8c3a5f0e28e28628027d0e071c4a5
Andy Jeffries <andy@andyjeffries.co.uk>
2016-02-05 08:32:54 +0000

    Changed firewall rule creation to a post instead of a put

--- app/models/civo/firewall_rule.rb
+++ app/models/civo/firewall_rule.rb
@@ -1,7 +1,7 @@
 module Civo
   class FirewallRule < Base
     get :all, "/v1/firewalls/:name/rules", required: [:name]
-    put :create, "/v1/firewalls/:name/rules", required: [:name, :protocol, :start_port,
+    post :create, "/v1/firewalls/:name/rules", required: [:name, :protocol, :start_port,
       :cidr, :direction]
     delete :remove, "/v1/firewalls/:name/rules/:id", required: [:name, :id]
   end
