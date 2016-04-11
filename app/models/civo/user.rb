1b63e58f55c11e3a10cf5eda0ecd3c009fcb34d4
Andy Jeffries <andy@andyjeffries.co.uk>
2016-04-11 08:37:48 +0100

    Added User.token to get a token by email address

--- app/models/civo/user.rb
+++ app/models/civo/user.rb
@@ -4,6 +4,7 @@ module Civo
     post :create, "/v1/users", required: %i{first_name last_name email_address}
     put :confirm, "/v1/users/confirm/:token"
     put :password, "/v1/users/password/:token"
+    get :token, "/v1/users/token"
     post :login, "/v1/users/login"
     put :flags, "/v1/users/:id/flags"
     put :update, "/v1/users/:id"
