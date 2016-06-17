c14423ae43cdf3fc74b8533eefa55b7e86b477ed
Andy Jeffries <andy@andyjeffries.co.uk>
2016-06-17 15:26:17 +0100

    Reworked v2 network API

--- app/models/civo/network.rb
+++ app/models/civo/network.rb
@@ -1,8 +1,15 @@
 module Civo
   class Network < Base
-    get :all, "/v#{ENV["CIVO_API_VERSION"] || "1"}/networks"
-    post :create, "/v#{ENV["CIVO_API_VERSION"] || "1"}/networks", required: [:name]
-    put :update, "/v#{ENV["CIVO_API_VERSION"] || "1"}/networks/:name", required: [:name]
-    delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "1"}/networks/:name", required: [:name]
+    if ENV["CIVO_API_VERSION"] == "2"
+      get :all, "/v2/networks"
+      post :create, "/v2/networks", required: [:label]
+      put :update, "/v2/networks/:id", required: [:id]
+      delete :remove, "/v2/networks/:id", required: [:id]
+    else
+      get :all, "/v1/networks"
+      post :create, "/v1/networks", required: [:name]
+      put :update, "/v1/networks/:name", required: [:name]
+      delete :remove, "/v1/networks/:name", required: [:name]
+    end
   end
 end
