6a0975fed2ed8708a6c93c03f8e2b57e51831ad0
Andy Jeffries <andy@andyjeffries.co.uk>
2016-06-21 16:30:40 +0100

    Added Firewall update

--- app/models/civo/firewall.rb
+++ app/models/civo/firewall.rb
@@ -3,5 +3,8 @@ module Civo
     get :all, "/v#{ENV["CIVO_API_VERSION"] || "1"}/firewalls"
     post :create, "/v#{ENV["CIVO_API_VERSION"] || "1"}/firewalls", required: [:name]
     delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "1"}/firewalls/:name", required: [:name]
+    if ENV["CIVO_API_VERSION"] == "2"
+      put :update, "/v2/firewalls/:id"
+    end
   end
 end
