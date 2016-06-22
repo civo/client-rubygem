4be71818af1b5ed6b15c9897d919d406ecd4542b
Andy Jeffries <andy@andyjeffries.co.uk>
2016-06-22 10:13:08 +0100

    Updated FirewalL Rules API to correctly use ID for v2

--- app/models/civo/firewall_rule.rb
+++ app/models/civo/firewall_rule.rb
@@ -1,8 +1,15 @@
 module Civo
   class FirewallRule < Base
-    get :all, "/v#{ENV["CIVO_API_VERSION"] || "1"}/firewalls/:name/rules", required: [:name]
-    post :create, "/v#{ENV["CIVO_API_VERSION"] || "1"}/firewalls/:name/rules", required: [:name, :protocol, :start_port,
-      :cidr, :direction]
-    delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "1"}/firewalls/:name/rules/:id", required: [:name, :id]
+    if ENV["CIVO_API_VERSION"] == "2"
+      get :all, "/v2/firewalls/:firewall_id/rules", required: [:firewall_id]
+      post :create, "/v2/firewalls/:firewall_id/rules", required: [:firewall_id, :protocol, :start_port,
+        :cidr, :direction]
+      delete :remove, "/v2/firewalls/:firewall_id/rules/:id", required: [:firewall_id, :id]
+    else
+      get :all, "/v#{ENV["CIVO_API_VERSION"] || "1"}/firewalls/:name/rules", required: [:name]
+      post :create, "/v#{ENV["CIVO_API_VERSION"] || "1"}/firewalls/:name/rules", required: [:name, :protocol, :start_port,
+        :cidr, :direction]
+      delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "1"}/firewalls/:name/rules/:id", required: [:name, :id]
+    end
   end
 end
