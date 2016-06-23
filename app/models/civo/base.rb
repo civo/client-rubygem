45b6c04cfe82dd28e579f4b88d04d57ec3de7dbc
Andy Jeffries <andy@andyjeffries.co.uk>
2016-06-23 10:02:37 +0100

    Added user switching functionality in to the client

--- app/models/civo/base.rb
+++ app/models/civo/base.rb
@@ -1,10 +1,29 @@
 module Civo
   class Base < ::Flexirest::Base
-    before_request do |name, request|
+    before_request :authorization_token
+
+    if ENV["CIVO_API_VERSION"] == "2"
+      before_request :administration_set_user
+    end
+
+    private
+
+    def authorization_token(name, request)
       request.headers["Authorization"] = "bearer #{ENV["CIVO_TOKEN"]}"
     end
+
+    def administration_set_user(name, request)
+      user_id = request.post_params[:user_id] # Don't delete the param in case it's needed by an action
+      if user_id.present?
+        request.headers["X-Civo-UserID"] = user_id
+      end
+      account_id = request.post_params[:account_id] # Don't delete the param in case it's needed by an action
+      if account_id.present?
+        request.headers["X-Civo-AccountID"] = account_id
+      end
+    end
+
   end
 end
 
 Flexirest::Base.base_url = ENV["CIVO_URL"]
-Flexirest::Base.verbose! if ENV["CIVO_VERBOSE"]
