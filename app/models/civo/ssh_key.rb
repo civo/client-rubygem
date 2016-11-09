fc50446afe768ec7fee60f27093211c4373e50da
Andy Jeffries <andy@andyjeffries.co.uk>
2016-11-09 15:53:53 +0000

    Added SSH Key update action, and fixed SSH Key delete action

--- app/models/civo/ssh_key.rb
+++ app/models/civo/ssh_key.rb
@@ -2,7 +2,8 @@ module Civo
   class SshKey < Base
     get :all, "/v#{ENV["CIVO_API_VERSION"] || "1"}/sshkeys"
     post :create, "/v#{ENV["CIVO_API_VERSION"] || "1"}/sshkeys", requires: [:name, :public_key]
-    delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "1"}/sshkeys/:name", requires: [:name]
+    delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "1"}/sshkeys/:id", requires: [:id]
+    put :update, "/v#{ENV["CIVO_API_VERSION"] || "1"}/sshkeys/:id", requires: [:id, :name]
 
     def to_partial_path
       "civo/ssh_key"
