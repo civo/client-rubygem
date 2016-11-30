1c556363c2a186357e22d052577c29ef43d00f33
Andy Jeffries <andy@andyjeffries.co.uk>
2016-11-30 09:44:08 +0000

    Corrected URLs for Firewall deletion

--- app/models/civo/firewall.rb
+++ app/models/civo/firewall.rb
@@ -2,9 +2,11 @@ module Civo
   class Firewall < Base
     get :all, "/v#{ENV["CIVO_API_VERSION"] || "1"}/firewalls"
     post :create, "/v#{ENV["CIVO_API_VERSION"] || "1"}/firewalls", required: [:name]
-    delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "1"}/firewalls/:name", required: [:name]
     if ENV["CIVO_API_VERSION"] == "2"
       put :update, "/v2/firewalls/:id"
+      delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "1"}/firewalls/:id", required: [:id]
+    else
+      delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "1"}/firewalls/:name", required: [:name]
     end
 
     def to_partial_path
