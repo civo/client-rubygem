1777d3829e663705c4d50489eae5e845dafe4871
Andy Jeffries <andy@andyjeffries.co.uk>
2016-12-04 21:12:40 +0000

    Added admin-only High CPU call

--- app/models/civo/instance.rb
+++ app/models/civo/instance.rb
@@ -1,6 +1,7 @@
 module Civo
   class Instance < Base
     get :all, "/v#{ENV["CIVO_API_VERSION"] || "1"}/instances"
+    get :high_cpu, "/v#{ENV["CIVO_API_VERSION"] || "1"}/instances/high_cpu"
     get :find, "/v#{ENV["CIVO_API_VERSION"] || "1"}/instances/:id", requires: [:id]
     post :create, "/v#{ENV["CIVO_API_VERSION"] || "1"}/instances", requires: [:hostname, :size, :region],
       defaults: {public_ip: true, template: "ubuntu-14.04", initial_user: "civo"}, timeout: 30
