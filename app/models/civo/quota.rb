8f5fc923863896b71a61cc031b9e01a9a16c2fd1
Andy Jeffries <andy@andyjeffries.co.uk>
2016-12-15 15:32:23 +0000

    Updated to include new admin-only quota functions

--- app/models/civo/quota.rb
+++ app/models/civo/quota.rb
@@ -4,6 +4,8 @@ module Civo
     put :update, "/v#{ENV["CIVO_API_VERSION"] || "1"}/quota/:name", requires: [:name]
     if ENV["CIVO_API_VERSION"] == "2"
       get :all, "/v2/quotas"
+      get :find, "/v#{ENV["CIVO_API_VERSION"] || "1"}/quota/:id", requires: [:id]
+      put :save, "/v#{ENV["CIVO_API_VERSION"] || "1"}/quota/:id", requires: [:id]
     end
 
     def to_partial_path
--- lib/civo/version.rb
+++ lib/civo/version.rb
@@ -1,3 +1,3 @@
 module Civo
-  VERSION = "0.7.8"
+  VERSION = "0.7.9"
 end
