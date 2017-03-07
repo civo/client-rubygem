437f952a56958d26d3fe4641a22c49935b051730
Andy Jeffries <andy@andyjeffries.co.uk>
2017-03-07 17:02:37 +0000

    Fixed broken URL

--- app/models/civo/account.rb
+++ app/models/civo/account.rb
@@ -2,7 +2,7 @@ module Civo
   class Account < Base
     get :all, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts"
     get :find, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts/:id", requires: [:id]
-    get :stats, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts/:id/stags", requires: [:id]
+    get :stats, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts/:id/stats", requires: [:id]
     post :create, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts", requires: [:name]
     put :reset, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts/:name", requires: [:name]
     put :save, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts"
