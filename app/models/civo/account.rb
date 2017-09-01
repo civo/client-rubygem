c45a6134a5bde6610eb5e1d550760220e046545d
Andy Jeffries <andy@andyjeffries.co.uk>
2017-09-01 13:38:15 +0100

    Correcting the admin-only account removal URL

--- app/models/civo/account.rb
+++ app/models/civo/account.rb
@@ -6,7 +6,7 @@ module Civo
     post :create, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts", requires: [:name]
     put :reset, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts/:id", requires: [:id]
     put :save, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts"
-    delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts/:name", requires: [:name]
+    delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts/:id", requires: [:id]
 
     def to_partial_path
       "civo/account"
