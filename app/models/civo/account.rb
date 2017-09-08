8652c6f473eafe2ec3f4a6eabd4217c908ac8a50
Andy Jeffries <andy@andyjeffries.co.uk>
2017-09-08 16:30:19 +0100

    Correcting save account call

--- app/models/civo/account.rb
+++ app/models/civo/account.rb
@@ -5,7 +5,7 @@ module Civo
     get :stats, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts/stats", requires: [:account_id]
     post :create, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts", requires: [:name]
     put :reset, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts/:id", requires: [:id]
-    put :save, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts"
+    put :save, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts/:id"
     delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts/:id", requires: [:id], send_delete_body: true
 
     def to_partial_path
