747e3653355ef960298c360812e4492c14bbfd66
Andy Jeffries <andy@andyjeffries.co.uk>
2017-09-28 10:17:10 +0100

    Load Balancer class done

---
+++ app/models/civo/loadbalancer.rb
@@ -0,0 +1,12 @@
+module Civo
+  class DnsDomain < Base
+    get :all, "/v2/loadbalancers"
+    post :create, "/v2/loadbalancers", required: [:hostname]
+    put :update, "/v2/loadbalancers/:id"
+    delete :remove, "/v2/loadbalancers/:id", required: [:id], send_delete_body: true
+
+    def to_partial_path
+      "civo/loadbalancer"
+    end
+  end
+end
