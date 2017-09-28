6498f6402399ae771d75a62b2fe8a80b78d835a1
Andy Jeffries <andy@andyjeffries.co.uk>
2017-09-28 10:21:06 +0100

    Adding loadbalancer find

--- app/models/civo/loadbalancer.rb
+++ app/models/civo/loadbalancer.rb
@@ -1,6 +1,7 @@
 module Civo
   class DnsDomain < Base
     get :all, "/v2/loadbalancers"
+    get :find, "/v2/loadbalancers/:id"
     post :create, "/v2/loadbalancers", required: [:hostname]
     put :update, "/v2/loadbalancers/:id"
     delete :remove, "/v2/loadbalancers/:id", required: [:id], send_delete_body: true
