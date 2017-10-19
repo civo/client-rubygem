3b93217fb8721f021f45d146b9647f7e9c2aa9b1
Andy Jeffries <andy@andyjeffries.co.uk>
2017-10-19 09:42:47 +0100

    Changing delete snapshot call

--- app/models/civo/snapshot.rb
+++ app/models/civo/snapshot.rb
@@ -3,7 +3,8 @@ module Civo
     get :all, "/v#{ENV["CIVO_API_VERSION"] || "1"}/snapshots"
     get :find, "/v#{ENV["CIVO_API_VERSION"] || "1"}/snapshots/:name", required: [:name]
     put :create, "/v#{ENV["CIVO_API_VERSION"] || "1"}/snapshots/:name", required: [:name, :instance_id]
-    delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "1"}/snapshots/:name", required: [:name], send_delete_body: true
+    delete :remove_old, "/v#{ENV["CIVO_API_VERSION"] || "1"}/snapshots/:name", required: [:name], send_delete_body: true
+    delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "1"}/snapshots/:id", required: [:id], send_delete_body: true
 
     def to_partial_path
       "civo/snapshot"
