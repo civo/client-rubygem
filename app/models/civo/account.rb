c2e3f78d5047090b8de3bdfd312a4c7175db71b5
Paul Kenrick <paul@absolutedevops.io>
2018-10-29 15:24:57 +0000

    Refactored new calls supporting single signup.

--- app/models/civo/account.rb
+++ app/models/civo/account.rb
@@ -5,8 +5,7 @@ module Civo
     get :stats, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts/stats", requires: [:account_id]
     post :create, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts", requires: [:name]
     put :confirm, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts/confirm/:token"
-    put :fraud_stage_one_complete, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts/fraud_stage_one_complete/:id", requires: [:id, :result]
-    put :fraud_stage_two_complete, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts/fraud_stage_two_complete/:id", requires: [:id, :result]
+    put :fraud_complete, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts/fraud/:id", requires: [:id, :stage, :result]
     put :reset, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts/:id", requires: [:id]
     put :save, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts/:id"
     delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "1"}/accounts/:id", requires: [:id], send_delete_body: true
