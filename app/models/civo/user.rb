3865529bf71d4b4727a9cd6cf93fad7e639160d2
Niall Duggan <niall@absolutedevops.io>
2018-11-21 14:57:58 +0000

    Remove user#confirm endpoint as it has been moved to account

--- app/models/civo/user.rb
+++ app/models/civo/user.rb
@@ -2,7 +2,6 @@ module Civo
   class User < Base
     get :ping, "/v#{ENV["CIVO_API_VERSION"] || "1"}/ping"
     post :create, "/v#{ENV["CIVO_API_VERSION"] || "1"}/users", required: %i{first_name last_name email_address}
-    put :confirm, "/v#{ENV["CIVO_API_VERSION"] || "1"}/users/confirm/:token"
     put :password, "/v#{ENV["CIVO_API_VERSION"] || "1"}/users/password/:token"
     post :token, "/v#{ENV["CIVO_API_VERSION"] || "1"}/users/token"
     post :login, "/v#{ENV["CIVO_API_VERSION"] || "1"}/users/login"
