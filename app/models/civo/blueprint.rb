21d9065a1c8cc72862352e3ee2b4eb5274ecf3f7
Paul Kenrick <paul@absolutedevops.io>
2019-03-04 12:04:09 +0000

    Adding paths for blueprint endpoints.

---
+++ app/models/civo/blueprint.rb
@@ -0,0 +1,14 @@
+module Civo
+  class Blueprint < Base
+    get :all, "/v#{ENV["CIVO_API_VERSION"] || "1"}/blueprints"
+    post :create, "/v#{ENV["CIVO_API_VERSION"] || "1"}/blueprints", requires: [:name, :dsl_content, :template_id]
+    put :update, "/v#{ENV["CIVO_API_VERSION"] || "1"}/blueprints/:id", requires: [:id]
+    put :start, "/v#{ENV["CIVO_API_VERSION"] || "1"}/blueprints/:id/start", requires: [:id]
+    put :stop, "/v#{ENV["CIVO_API_VERSION"] || "1"}/blueprints/:id/stop", requires: [:id]
+    put :modify_template, "/v#{ENV["CIVO_API_VERSION"] || "1"}/blueprints/:id/modify_template", requires: [:id, :checksum, :volume_id]
+  end
+
+  def to_partial_path
+    "civo/blueprint"
+  end
+end
