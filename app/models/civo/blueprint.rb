cc0567048269324f29a87d7921560fbf62639e52
Niall Duggan <niall@absolutedevops.io>
2019-03-13 14:51:47 +0000

    Add #find and #remove to blueprints

--- app/models/civo/blueprint.rb
+++ app/models/civo/blueprint.rb
@@ -1,8 +1,10 @@
 module Civo
   class Blueprint < Base
     get :all, "/v#{ENV["CIVO_API_VERSION"] || "1"}/blueprints"
+    get :find, "/v#{ENV["CIVO_API_VERSION"] || "2"}/blueprints/:id", requires: [:id]
     post :create, "/v#{ENV["CIVO_API_VERSION"] || "1"}/blueprints", requires: [:name, :dsl_content, :template_id]
     put :update, "/v#{ENV["CIVO_API_VERSION"] || "1"}/blueprints/:id", requires: [:id]
+    delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "2"}/blueprints/:id", requires: [:id], send_delete_body: true
     put :start, "/v#{ENV["CIVO_API_VERSION"] || "1"}/blueprints/:id/start", requires: [:id]
     put :stop, "/v#{ENV["CIVO_API_VERSION"] || "1"}/blueprints/:id/stop", requires: [:id]
     put :modify_template, "/v#{ENV["CIVO_API_VERSION"] || "1"}/blueprints/:id/template", requires: [:id, :volume_id]
