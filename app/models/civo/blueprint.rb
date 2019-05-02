6c0c582d9f534c09dd6928a18a449cb2a3317978
Niall Duggan <niall@civo.com>
2019-05-02 12:05:41 +0100

    Replace blueprint #stop with #stage, #complete and #fail

--- app/models/civo/blueprint.rb
+++ app/models/civo/blueprint.rb
@@ -1,12 +1,14 @@
 module Civo
   class Blueprint < Base
-    get :all, "/v#{ENV["CIVO_API_VERSION"] || "1"}/blueprints"
+    get :all, "/v#{ENV["CIVO_API_VERSION"] || "2"}/blueprints"
     get :find, "/v#{ENV["CIVO_API_VERSION"] || "2"}/blueprints/:id", requires: [:id]
-    post :create, "/v#{ENV["CIVO_API_VERSION"] || "1"}/blueprints", requires: [:name, :dsl_content, :template_id]
-    put :update, "/v#{ENV["CIVO_API_VERSION"] || "1"}/blueprints/:id", requires: [:id]
+    post :create, "/v#{ENV["CIVO_API_VERSION"] || "2"}/blueprints", requires: [:name, :dsl_content, :template_id]
+    put :update, "/v#{ENV["CIVO_API_VERSION"] || "2"}/blueprints/:id", requires: [:id]
     delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "2"}/blueprints/:id", requires: [:id], send_delete_body: true
-    put :start, "/v#{ENV["CIVO_API_VERSION"] || "1"}/blueprints/:id/start", requires: [:id]
-    put :stop, "/v#{ENV["CIVO_API_VERSION"] || "1"}/blueprints/:id/stop", requires: [:id]
+    put :start, "/v#{ENV["CIVO_API_VERSION"] || "2"}/blueprints/:id/start", requires: [:id]
+    put :stage, "/v#{ENV["CIVO_API_VERSION"] || "2"}/blueprints/:id/stage", requires: [:id, :volume_id]
+    put :complete, "/v#{ENV["CIVO_API_VERSION"] || "2"}/blueprints/:id/complete", requires: [:id, :openstack_volume_id]
+    put :fail, "/v#{ENV["CIVO_API_VERSION"] || "2"}/blueprints/:id/fail", requires: [:id, :openstack_volume_id]
 
     def to_partial_path
       "civo/blueprint"
