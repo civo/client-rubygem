4ea6cb613e80a19f5c1f7ea58c421733cc308fd7
Andy Jeffries <andy@andyjeffries.co.uk>
2019-07-05 16:39:38 +0100

    Add complete/fail methods to Blueprint too

--- app/models/civo/blueprint.rb
+++ app/models/civo/blueprint.rb
@@ -5,6 +5,8 @@ module Civo
     post :create, "/v#{ENV["CIVO_API_VERSION"] || "2"}/blueprints", requires: [:name, :dsl_content, :template_id]
     put :update, "/v#{ENV["CIVO_API_VERSION"] || "2"}/blueprints/:id", requires: [:id]
     put :stage, "/v#{ENV["CIVO_API_VERSION"] || "2"}/blueprints/:id/stage", requires: [:id]
+    put :complete, "/v#{ENV["CIVO_API_VERSION"] || "2"}/blueprints/:id/complete", requires: [:id]
+    put :fail, "/v#{ENV["CIVO_API_VERSION"] || "2"}/blueprints/:id/fail", requires: [:id]
     delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "2"}/blueprints/:id", requires: [:id], send_delete_body: true
     put :start, "/v#{ENV["CIVO_API_VERSION"] || "2"}/blueprints/:id/start", requires: [:id]
     put :stop, "/v#{ENV["CIVO_API_VERSION"] || "2"}/blueprints/:id/stop", requires: [:id]
