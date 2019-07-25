47c4b53241ea58b84dc95c682cdb57d9500cfdf9
Andy Jeffries <andy@andyjeffries.co.uk>
2019-07-25 10:37:36 +0100

    Change k8s to kubernetes

--- app/models/civo/kubernetes.rb
+++ app/models/civo/kubernetes.rb
@@ -1,10 +1,10 @@
 module Civo
   class Kubernetes < Base
-    get :all, "/v#{ENV["CIVO_API_VERSION"] || "2"}/k8sclusters"
-    get :find, "/v#{ENV["CIVO_API_VERSION"] || "2"}/k8sclusters/:id", requires: [:id]
-    post :create, "/v#{ENV["CIVO_API_VERSION"] || "2"}/k8sclusters", requires: [:name]
-    put :update, "/v2/k8sclusters/:id"
-    delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "2"}/k8sclusters/:id", requires: [:id], send_delete_body: true
+    get :all, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetesclusters"
+    get :find, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetesclusters/:id", requires: [:id]
+    post :create, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetesclusters", requires: [:name]
+    put :update, "/v2/kubernetesclusters/:id"
+    delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetesclusters/:id", requires: [:id], send_delete_body: true
 
     def to_partial_path
       "civo/kubernetes"
