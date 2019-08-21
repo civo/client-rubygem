32f25c81ba8cadfe4843f134d8704bc680c07ec6
Andy Jeffries <andy@andyjeffries.co.uk>
2019-08-21 13:01:29 +0100

    Change Kubernetes endpoint URLs

--- app/models/civo/kubernetes.rb
+++ app/models/civo/kubernetes.rb
@@ -1,11 +1,11 @@
 module Civo
   class Kubernetes < Base
-    get :all, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetesclusters"
-    get :find, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetesclusters/:id", requires: [:id]
-    post :create, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetesclusters", requires: [:name]
-    post :recycle, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetesclusters/:id/recycle", requires: [:id, :hostname]
-    put :update, "/v2/kubernetesclusters/:id"
-    delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetesclusters/:id", requires: [:id], send_delete_body: true
+    get :all, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/clusters"
+    get :find, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/clusters/:id", requires: [:id]
+    post :create, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/clusters", requires: [:name]
+    post :recycle, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/clusters/:id/recycle", requires: [:id, :hostname]
+    put :update, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/clusters/:id"
+    delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/clusters/:id", requires: [:id], send_delete_body: true
 
     def to_partial_path
       "civo/kubernetes"
--- lib/civo/version.rb
+++ lib/civo/version.rb
@@ -1,3 +1,3 @@
 module Civo
-  VERSION = "1.2.6"
+  VERSION = "1.2.7"
 end
