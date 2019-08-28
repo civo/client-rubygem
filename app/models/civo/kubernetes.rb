0209aaeb0cf02d25e2eb3fc1850ea2d9cef840a3
Andy Jeffries <andy@andyjeffries.co.uk>
2019-08-28 13:54:25 +0100

    Add application parts to Kubernetes

--- app/models/civo/kubernetes.rb
+++ app/models/civo/kubernetes.rb
@@ -7,6 +7,9 @@ module Civo
     put :update, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/clusters/:id"
     delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/clusters/:id", requires: [:id], send_delete_body: true
 
+    get :applications, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/applications"
+    get :application, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/applications/:name", requires: [:name]
+
     def to_partial_path
       "civo/kubernetes"
     end
