e2e955cb1778ae491c1ec766d8a09b2f72797c51
Tahsin Hasan <tahsin352@yahoo.com>
2021-11-25 20:46:23 +0600

    Fake request for LB

--- app/models/civo/load_balancer.rb
+++ app/models/civo/load_balancer.rb
@@ -1,13 +1,25 @@
 module Civo
   class LoadBalancer < Base
-    get :all, "/v2/loadbalancers"
-    get :find, "/v2/loadbalancers/:id"
-    post :create, "/v2/loadbalancers", required: [:hostname]
-    put :update, "/v2/loadbalancers/:id"
-    delete :remove, "/v2/loadbalancers/:id", required: [:id]
+    get :all, "/v2/loadbalancers", fake: :loadbalancers
+    get :find, "/v2/loadbalancers/:id", fake: :get_loadbalancer
+    post :create, "/v2/loadbalancers", required: [:hostname], fake: :get_loadbalancer
+    put :update, "/v2/loadbalancers/:id", fake: :get_loadbalancer
+    delete :remove, "/v2/loadbalancers/:id", required: [:id], fake: :delete_loadbalancer
 
     def to_partial_path
       "civo/loadbalancer"
+    end
+
+    def loadbalancers
+      [{ id: 23, name: "Johnny", algorithm: "round_robin" }, { id: 223, name: "Dep", algorithm: "weighted_fixed" }].to_json
+    end
+
+    def get_loadbalancer
+      { id: 23, name: "Johnny", algorithm: "round_robin" }.to_json
+    end
+
+    def delete_loadbalancer
+      { result: "Ok" }.to_json
     end
   end
 end
