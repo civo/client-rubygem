c22bfbe6e0fb15b710a742010871ae1cf29e4965
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2022-05-16 10:09:30 +0100

    Add platform application commands
    * Add CRUD fro /applications endpoint
    
    * Add scaffold responses to platform apps
    
    * Update "platform" to "PlatformApplication"
    
    * api endpoint is applications
    
    * rename platform
    
    * Connection with API
    
    Co-authored-by: Tahsin Hasan <tahsin352@yahoo.com>

---
+++ app/models/civo/application.rb
@@ -0,0 +1,13 @@
+module Civo
+  class Application < Base
+    get :all, "/v#{ENV["CIVO_API_VERSION"] || "2"}/applications"
+    get :find, "/v#{ENV["CIVO_API_VERSION"] || "2"}/applications/:id", requires: [:id]
+    post :create, "/v#{ENV["CIVO_API_VERSION"] || "2"}/applications", requires: [:name, :region, :network_id, :size, :ssh_key_ids]
+    put :update, "/v#{ENV["CIVO_API_VERSION"] || "2"}/applications/:id", request_body_type: :json
+    delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "2"}/applications/:id", requires: [:id]
+
+    def to_partial_path
+      "civo/application"
+    end
+  end
+end
