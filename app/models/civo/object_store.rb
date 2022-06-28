b3de3bcffb6184f017b19c355779cd88f62f1828
Tahsin Hasan <tahsin352@yahoo.com>
2022-06-28 17:18:01 +0600

    add region

--- app/models/civo/object_store.rb
+++ app/models/civo/object_store.rb
@@ -1,10 +1,10 @@
 module Civo
   class ObjectStore < Base
     get :all, "/v2/objectstores", requires: [:region]
-    get :find, "/v2/objectstores/:id", requires: [:id]
-    post :create, "/v2/objectstores", requires: [:name, :max_size_gb, :max_objects]
-    put :update, "/v2/objectstores/:id", requires: [:id]
-    delete :remove, "/v2/objectstores/:id", requires: [:id]
+    get :find, "/v2/objectstores/:id", requires: [:id, :region]
+    post :create, "/v2/objectstores", requires: [:name, :max_size_gb, :region]
+    put :update, "/v2/objectstores/:id", requires: [:id, :region]
+    delete :remove, "/v2/objectstores/:id", requires: [:id, :region]
 
     def to_partial_path
       "civo/object_store"
