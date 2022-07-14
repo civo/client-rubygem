57c287135aff05f3676c94166def9e333c1aa0ba
Tahsin Hasan <tahsin352@yahoo.com>
2022-07-14 16:46:25 +0600

    Reserved IP

---
+++ app/models/civo/reserved_ip.rb
@@ -0,0 +1,14 @@
+module Civo
+  class ReservedIp < Base
+    get :all, "/v2/ips", requires: [:region]
+    get :find, "/v2/ips/:id", requires: [:id, :region]
+    post :create, "/v2/ips", requires: [:name, :region]
+    put :update, "/v2/ips/:id", requires: [:id, :region]
+    delete :remove, "/v2/ips/:id", requires: [:id, :region]
+    post :actions, "/v2/ips/:id/actions", requires: [:id, :region, :action]
+
+    def to_partial_path
+      "civo/ips"
+    end
+  end
+end
