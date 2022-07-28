c18b7872ac38ff2edebd1bc082f44e8a105061db
Tahsin Hasan <tahsin352@yahoo.com>
2022-07-28 12:32:07 +0600

    default value

--- app/models/civo/object_store.rb
+++ app/models/civo/object_store.rb
@@ -2,7 +2,7 @@ module Civo
   class ObjectStore < Base
     get :all, "/v2/objectstores", requires: [:region]
     get :find, "/v2/objectstores/:id", requires: [:id, :region]
-    post :create, "/v2/objectstores", requires: [:name, :max_size_gb, :region]
+    post :create, "/v2/objectstores", requires: [:name, :max_size_gb, :region], defaults: {max_size_gb: 500}
     put :update, "/v2/objectstores/:id", requires: [:id, :region]
     delete :remove, "/v2/objectstores/:id", requires: [:id, :region]
 
