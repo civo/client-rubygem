9e2dc0fc7a9c96295e80c8d3f9d4deede63825ff
Tahsin Hasan <tahsin352@yahoo.com>
2022-11-10 19:47:14 +0600

    Allocate release subnet

--- app/models/civo/subnet.rb
+++ app/models/civo/subnet.rb
@@ -3,6 +3,8 @@ module Civo
     get :all, "/v2/networks/:network_id/subnets", requires: [:region]
     post :create, "/v2/networks/:network_id/subnets", requires: [:name, :region]
     delete :remove, "/v2/networks/:network_id/subnets/:id", requires: [:id, :region]
+    post :allocate, "/v2/networks/:network_id/subnets/:id/routes", requires: [:id, :region]
+    delete :release, "/v2/networks/:network_id/subnets/:id/routes", requires: [:id, :region]
 
     def to_partial_path
       "civo/subnet"
