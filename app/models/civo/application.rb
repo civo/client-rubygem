2894b36eeb7fa5da93608a4b2f5fb72050c22b76
Tahsin Hasan <tahsin352@yahoo.com>
2023-02-01 18:01:25 +0600

    Update applications

--- app/models/civo/application.rb
+++ app/models/civo/application.rb
@@ -2,8 +2,8 @@ module Civo
   class Application < Base
     get :all, "/v#{ENV["CIVO_API_VERSION"] || "2"}/applications"
     get :find, "/v#{ENV["CIVO_API_VERSION"] || "2"}/applications/:id", requires: [:id]
-    post :create, "/v#{ENV["CIVO_API_VERSION"] || "2"}/applications", requires: [:name, :region, :network_id, :size, :ssh_key_ids]
-    put :update, "/v#{ENV["CIVO_API_VERSION"] || "2"}/applications/:id", request_body_type: :json
+    post :create, "/v#{ENV["CIVO_API_VERSION"] || "2"}/applications", requires: [:name, :region, :network_id, :size], request_body_type: :json
+    patch :update, "/v#{ENV["CIVO_API_VERSION"] || "2"}/applications/:id", request_body_type: :json
     delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "2"}/applications/:id", requires: [:id]
 
     def to_partial_path
