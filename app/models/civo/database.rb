24cfb5c0ed5f1a15a99c915909720c3cf98b7c65
Tahsin Hasan <tahsin352@yahoo.com>
2023-03-28 14:27:53 +0600

    Integrate pgsql in dbaas

--- app/models/civo/database.rb
+++ app/models/civo/database.rb
@@ -5,6 +5,7 @@ module Civo
     get :find, "/v#{ENV["CIVO_API_VERSION"] || "2"}/databases/:id", requires: [:id, :region]
     patch :update, "/v#{ENV["CIVO_API_VERSION"] || "2"}/databases/:id", requires: [:id, :region]
     delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "2"}/databases/:id", requires: [:id, :region]
+    get :versions, "/v#{ENV["CIVO_API_VERSION"] || "2"}/databases/versions", requires: [:region]
 
     def to_partial_path
       "civo/database"
