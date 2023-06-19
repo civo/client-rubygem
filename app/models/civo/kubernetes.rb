740ebfcc78f8ceb25754db1e4d26e0ba2182b1e6
Tahsin Hasan <tahsin352@yahoo.com>
2023-06-19 09:37:09 +0600

    Add and update labels and taints in node pool

--- app/models/civo/kubernetes.rb
+++ app/models/civo/kubernetes.rb
@@ -7,6 +7,9 @@ module Civo
     put :update, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/clusters/:id", request_body_type: :json
     delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/clusters/:id", requires: [:id]
 
+    post :create_pool, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/clusters/:cluster_id/pools", requires: [:cluster_id, :id], request_body_type: :json
+    put :update_pool, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/clusters/:cluster_id/pools/:id", requires: [:cluster_id, :id], request_body_type: :json
+
     get :applications, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/applications"
     get :application, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/applications/:name", requires: [:name]
 
