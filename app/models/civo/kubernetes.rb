20f0b2344f08afae174a52c7ef99eb16f6e2ba40
Tahsin Hasan <tahsin352@yahoo.com>
2023-07-20 20:53:22 +0600

    Enable public ip on node pool

--- app/models/civo/kubernetes.rb
+++ app/models/civo/kubernetes.rb
@@ -2,7 +2,7 @@ module Civo
   class Kubernetes < Base
     get :all, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/clusters"
     get :find, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/clusters/:id", requires: [:id]
-    post :create, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/clusters", requires: [:name]
+    post :create, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/clusters", requires: [:name], request_body_type: :json
     post :recycle, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/clusters/:id/recycle", requires: [:id, :hostname]
     put :update, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/clusters/:id", request_body_type: :json
     delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "2"}/kubernetes/clusters/:id", requires: [:id]
