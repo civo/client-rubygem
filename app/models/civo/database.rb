da1804096f9641e36bc1ab12f004f07a85952bcc
Tahsin Hasan <tahsin352@yahoo.com>
2023-10-02 13:49:48 +0600

    Add backup support in postgresql

--- app/models/civo/database.rb
+++ app/models/civo/database.rb
@@ -5,6 +5,10 @@ module Civo
     get :find, "/v#{ENV["CIVO_API_VERSION"] || "2"}/databases/:id", requires: [:id, :region]
     patch :update, "/v#{ENV["CIVO_API_VERSION"] || "2"}/databases/:id", requires: [:id, :region]
     delete :remove, "/v#{ENV["CIVO_API_VERSION"] || "2"}/databases/:id", requires: [:id, :region]
+    get :list_backups, "/v#{ENV["CIVO_API_VERSION"] || "2"}/databases/:id/backups", requires: [:id, :region]
+    post :create_backup, "/v#{ENV["CIVO_API_VERSION"] || "2"}/databases/:id/backups", requires: [:id, :region]
+    put :update_backup, "/v#{ENV["CIVO_API_VERSION"] || "2"}/databases/:id/backups", requires: [:id, :region]
+    post :restore, "/v#{ENV["CIVO_API_VERSION"] || "2"}/databases/:id/restore", requires: [:id, :region]
     get :versions, "/v#{ENV["CIVO_API_VERSION"] || "2"}/databases/versions", requires: [:region]
 
     def to_partial_path
