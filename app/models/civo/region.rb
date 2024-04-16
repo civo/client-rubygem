b02f913c86efcc190092162144786f64f00abce7
Tahsin Hasan <tahsin352@yahoo.com>
2024-04-16 21:20:50 +0600

    gpu availability check (#32)

--- app/models/civo/region.rb
+++ app/models/civo/region.rb
@@ -1,6 +1,7 @@
 module Civo
   class Region < Base
     get :all, "/v#{ENV["CIVO_API_VERSION"] || "2"}/regions"
+    get :availability, "/v#{ENV["CIVO_API_VERSION"] || "2"}/regions/availability"
 
     def to_partial_path
       "civo/region"
